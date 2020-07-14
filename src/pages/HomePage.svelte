c891c4ab669c6c325832342afd0a9b45d97bf3b3
Razvan <tangent.jotey@gmail.com>
2020-07-14 13:52:30 +0200

    first commit

---
+++ src/pages/HomePage.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let location;
+    console.log(location);
+</script>
+
+<a href="https://metroui.org.ua" target="_blank">
+    <h1 class="text-center">Metro 4</h1>
+    <h3 class="text-center">The Components Library</h3>
+    <div data-role="cube"></div>
+</a>
